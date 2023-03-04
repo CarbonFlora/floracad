ef226d40aac6e6fc35347c334ca4f752b7f11e8e
CarbonFlora <zihaoliang0413@gmail.com>
2023-03-03 16:36:03 -0800

    finished parsing input.md file

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,65 @@
-struct HorizontalCurve {
+use eqsolver::single_variable::FDNewton;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, Error};
 
+struct HorizontalCurve {
+    dimensions: HorizontalDimensions,
+    stations: HorizontalCriticalStations,
 }
 
 struct HorizontalCriticalStations {
-    pc: i32,
+    pc: i32, 
     pi: i32,
     pt: i32,
 
 }
 
 struct HorizontalDimensions {
-    radius: i32,
-    curve_length: i32,
-    tangent_distance: i32,
-    long_chord: i32,
-    middle_ordinate: i32,
-    external: i32,
-    curve_length_100: i32,
-    curve_angle: angle,
+    radius: f64,
+    curve_length: f64,
+    tangent_distance: f64,
+    long_chord: f64,
+    middle_ordinate: f64,
+    external: f64,
+    curve_length_100: f64, //(Da)
+    curve_angle: f64, //radians (I)
 }
 
-struct angle {
-    degrees: i32,
-    radians: i32,
+fn single_var() {
+    let y = 32.0;
+    //let f = |x: f64| x.exp() - 1./x; // e^x = 1/x
+    let f = |x: f64| x - y*2.0; // e^x = 1/x
+    let solution = FDNewton::new(f).solve(0.);
+    println!("Solution: {:?}", solution);
 }
 
-impl angle {
-    fn init (&mut self) {
+fn parse_input() -> Result<HashMap<String, String>, Error> {
+    let input = File::open("input.md")?;
+    let buffered = BufReader::new(input);
+    let mut arguments = HashMap::new();
+
+    for lines in buffered.lines() {
+        if let Ok(line) = lines {
+            let i = line.split_once('=');
+            match i {
+                None => continue,
+                Some(args) => arguments.insert(args.0.to_owned(), args.1.to_owned()),
+            };
+        } else {
+            panic!("Failed at parsing input.md file.");
+        }
     }
+
+    //arguments.insert("radius", Some(3.0));
+    Ok(arguments)
 }
+
+
+
+/*The general idea:
+If given any of the not Da, I, PI details, convert the given information from a .md document
+to either Da, I, or PI. From there you can figure out everything else. */
 
 #[cfg(test)]
 mod tests {
@@ -36,5 +67,7 @@ mod tests {
 
     #[test]
     fn it_works() {
+        println!("{:#?}", parse_input());
+        assert!(false);
     }
 }
