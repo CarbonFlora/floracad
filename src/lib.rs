a0233a67266ebac6c6d14315a14d394692d77f71
CarbonFlora <zihaoliang0413@gmail.com>
2023-03-03 20:18:39 -0800

    finished angle conversions

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,56 @@
 use eqsolver::single_variable::FDNewton;
+use dms_coordinates::DMS;
 use std::collections::HashMap;
+use std::f64::consts::PI;
 use std::fs::File;
 use std::io::{Write, BufReader, BufRead, Error};
 
-struct HorizontalCurve {
+#[derive(Debug)]
+pub enum Angle {
+    decimal_degrees (f64),
+    dms (DMS),
+    radians (f64),
+}
+
+impl Angle {
+    pub fn to_dms(self) -> Self {
+        match self {
+            Angle::decimal_degrees(ddeg) => Angle::dms(DMS::from_decimal_degrees(ddeg, false)),
+            Angle::radians(rad) => Angle::dms(DMS::from_decimal_degrees(rad*180.0/PI, false)),
+            _ => return self,
+        }
+    }
+
+    pub fn to_decimal_degrees(self) -> Self {
+        match self {
+            Angle::dms(dms_value) => Angle::decimal_degrees(DMS::to_decimal_degrees(&dms_value)),
+            Angle::radians(rad) => Angle::decimal_degrees(rad*180.0/PI),
+            _ => return self,
+        }
+    }
+
+    pub fn to_radians(self) -> Self {
+        match self {
+            Angle::dms(dms_value) => Angle::radians(DMS::to_decimal_degrees(&dms_value)*PI/180.0),
+            Angle::decimal_degrees(dd_value) => Angle::radians(dd_value*PI/180.0),
+            _ => return self,
+        }
+    }
+}
+
+pub struct HorizontalCurve {
     dimensions: HorizontalDimensions,
     stations: HorizontalCriticalStations,
 }
 
-struct HorizontalCriticalStations {
+pub struct HorizontalCriticalStations {
     pc: i32, 
     pi: i32,
     pt: i32,
 
 }
 
-struct HorizontalDimensions {
+pub struct HorizontalDimensions {
     radius: f64,
     curve_length: f64,
     tangent_distance: f64,
@@ -23,10 +58,10 @@ struct HorizontalDimensions {
     middle_ordinate: f64,
     external: f64,
     curve_length_100: f64, //(Da)
-    curve_angle: f64, //radians (I)
+    curve_angle: Angle, //radians (I)
 }
 
-fn single_var() {
+pub fn single_var() {
     let y = 32.0;
     //let f = |x: f64| x.exp() - 1./x; // e^x = 1/x
     let f = |x: f64| x - y*2.0; // e^x = 1/x
@@ -34,7 +69,7 @@ fn single_var() {
     println!("Solution: {:?}", solution);
 }
 
-fn parse_input() -> Result<HashMap<String, String>, Error> {
+pub fn parse_input() -> Result<HashMap<String, String>, Error> {
     let input = File::open("input.md")?;
     let buffered = BufReader::new(input);
     let mut arguments = HashMap::new();
@@ -56,18 +91,6 @@ fn parse_input() -> Result<HashMap<String, String>, Error> {
 }
 
 
-
 /*The general idea:
 If given any of the not Da, I, PI details, convert the given information from a .md document
 to either Da, I, or PI. From there you can figure out everything else. */
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        println!("{:#?}", parse_input());
-        assert!(false);
-    }
-}
---
+++ tests/tests.rs
@@ -0,0 +1,46 @@
+#[cfg(test)]
+mod tests {
+    use horizontal_alignment::{*};
+    use dms_coordinates::{DMS, Bearing::*};
+
+    #[test]
+    fn it_works() {
+        println!("{:#?}", parse_input());
+        assert!(true);
+    }
+
+    #[test]
+    fn test_dms() {
+        let dd_value = 15.0169444444444;
+        println!("DD: {:?}", dd_value);
+        println!("DMS: {:?}", DMS::from_decimal_degrees(dd_value, false));
+        assert!(true);
+    }
+
+    #[test]
+    fn test_to_dms() {
+        let rad = Angle::radians(1.0);
+        let dd_value = Angle::decimal_degrees(1.4687);
+        println!("{:?}", rad.to_dms());
+        println!("{:?}", dd_value.to_dms());
+        assert!(true);
+    }
+
+    #[test]
+    fn test_to_dd() {
+        let rad = Angle::radians(1.0);
+        let dms_1 = Angle::dms(DMS{degrees: 15, minutes: 1, seconds: 1.0, bearing: East});
+        println!("{:?}", rad.to_decimal_degrees());
+        println!("{:?}", dms_1.to_decimal_degrees());
+        assert!(true);
+    }
+
+    #[test]
+    fn test_to_radians() {
+        let dd_value = Angle::decimal_degrees(1.0);
+        let dms_1 = Angle::dms(DMS{degrees: 15, minutes: 1, seconds: 1.0, bearing: East});
+        println!("{:?}", dd_value.to_radians());
+        println!("{:?}", dms_1.to_radians());
+        assert!(true);
+    }
+}
